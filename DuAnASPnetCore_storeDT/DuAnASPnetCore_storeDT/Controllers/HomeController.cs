d7dc551566ab63fdb7fce69c3868c196b0456f60
LeeNhuNghia <lnghia637@gmail.com>
2023-09-26 21:32:53 +0700

    v5_

--- DuAnASPnetCore_storeDT/DuAnASPnetCore_storeDT/Controllers/HomeController.cs
+++ DuAnASPnetCore_storeDT/DuAnASPnetCore_storeDT/Controllers/HomeController.cs
@@ -12,15 +12,19 @@ namespace DuAnASPnetCore_storeDT.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly QLStoreCellPhoneContext db;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, QLStoreCellPhoneContext _db)
         {
             _logger = logger;
+            db = _db;
         }
 
-        public IActionResult Index()
+        public IActionResult Index(string idcat)
         {
-            return View();
+            var idTL = idcat == null ? "DT" : idcat;
+            var lstCat = db.Products.Where(w => w.CategoryId == idTL).Take(5).ToList();
+            return View(lstCat);
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
---
+++ DuAnASPnetCore_storeDT/DuAnASPnetCore_storeDT/Controllers/ProductController.cs
@@ -0,0 +1,29 @@
+﻿using DuAnASPnetCore_storeDT.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DuAnASPnetCore_storeDT.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly QLStoreCellPhoneContext db;
+
+        public ProductController(QLStoreCellPhoneContext _db)
+        {
+            db = _db;
+        }
+
+        // GET: ProductController/Details/5
+        public ActionResult Details(int idprd)
+        {
+            var detailprd = db.Products.Find(idprd);
+            return View(detailprd);
+        }
+
+        
+    }
+}
